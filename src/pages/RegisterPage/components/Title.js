9cf6eab02f7bd5ea84439b9028d31349e75f76ce
Keyi <likkey1@gmail.com>
2021-11-24 15:40:07 +0100

    version1.2 register++

---
+++ src/pages/RegisterPage/components/Title.js
@@ -0,0 +1,14 @@
+import Logo from "../../logos/Logo";
+
+const Title = () => {
+    return <div>
+        <h1 className="Title_Set">Create Your </h1>
+        <h2 className="TECO_LAB_TEXT" id="KIT_GREEN">TECO-LAB
+            <span> Account </span></h2>
+
+
+        <Logo/>
+    </div>
+}
+
+export default Title
---
+++ src/pages/RegisterPage/register.js
@@ -0,0 +1,12 @@
+import RegisterItem from "./components/registerItem";
+import Title from "./components/Title";
+
+
+const Register = () => {
+    return <div>
+        <Title/>
+        <RegisterItem/>
+    </div>
+}
+
+export default Register
