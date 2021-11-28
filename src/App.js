55a3c45fc9a59e7770fdf0efcfd8f1d016a82cfa
likkey <likkey1@gmail.com>
2021-11-28 01:16:33 +0100

    version 2.0 get/post API in registerItem.js implementation.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App'
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/WelcomePage/home";
 import Register from "./pages/RegisterPage/register";
 import Login from "./pages/LoginPage/login";
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
     return (
 
         <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route path="register" element={<Register/>}/>
-            <Route path="Login" element={<Login/>}/>
+            <Route exact path="/" element={<Home />} />
+            <Route path="register" element={<Register />} />
+            <Route path="Login" element={<Login />} />
         </Routes>
+
     );
 }
 
--- src/pages/RegisterPage/components/registerItem.js
+++ src/pages/RegisterPage/components/registerItem.js
@@ -1,34 +1,74 @@
+import axios from 'axios';
+import React from 'react';
 import '../register.css'
 
+const baseUrl = "https://localhost:8086/users";
+
+
+
 const RegisterItem = () => {
-    return <div className="spacing">
 
-        <div className="email">
-            <label form="email">E-mail </label>
-            <input type="email"/>
+    const [post, setPost] = React.useState(null);
 
-            <button className="verify">verify</button>
+    React.useEffect(() => {
+        axios.get("http://localhost:8086/users/getTestUser").then((res) => {
+            console.log(res);
+            setPost(res.data.data);
+        })
+    }, [])
+
+
+    function createPost() {
+        axios.post("http://localhost:8086/users/insertUserTest", {
+            userId: post.userId,
+            userPassword: post.userPassword,
+            userRole: post.userRole,
+            verifyCode: post.verifyCode,
+            userName: post.userName,
+            email: post.email,
+            realName: post.realName
+
+        })
+            .then((res) => {
+                console.log(res)
+                setPost(res.data.data);
+            });
+    }
+
+
+
+    return (
+         <div className="spacing">
+
+            <div className="email">
+                <label form="email">E-mail </label>
+                <input type="email" />
+
+                <button className="verify">verify</button>
+            </div>
+
+            <div className="password">
+                <label form="password">Password</label>
+                <input type="password" />
+            </div>
+
+            <div className="password_repeat">
+                <label>Password repeat</label>
+                <input type="password" />
+            </div>
+
+            <div className="name">
+                <label>Name</label>
+                <input type="type" />
+            </div>
+
+
+            <h1>hello world! </h1>
+            <button className="register_Button" onClick={createPost}>Register</button>
+
         </div>
 
-        <div className="password">
-            <label form="password">Password</label>
-            <input type="password"/>
-        </div>
-
-        <div className="password_repeat">
-            <label>Password repeat</label>
-            <input type="password"/>
-        </div>
-
-        <div className="name">
-            <label>Name</label>
-            <input type="type"/>
-        </div>
-
-        <button className="register_Button">Register</button>
-
-    </div>
-
+    )
 }
 
 export default RegisterItem
