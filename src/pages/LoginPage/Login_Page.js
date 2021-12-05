96590340c760042e2a95e7fe7c1fb37fa80a43e9
Haotian Wu <haotianwuwinston@gmail.com>
2021-12-05 23:03:08 +0100

    standardize the name of class and components

--- src/pages/LoginPage/Login_Page.js
+++ src/pages/LoginPage/Login_Page.js
@@ -1,7 +1,7 @@
 
 
 import LOGIN_PAGE_FORM from "./components/Login_Page_Form";
-
+import 'Login_Page_Style.css'
 import LOGIN_PAGE_TEXT from "./components/Login_Page_Text";
 import LOGO from "../logos/Logo";
 
--- src/pages/RegisterPage/Register_Page.js
+++ src/pages/RegisterPage/Register_Page.js
@@ -1,7 +1,7 @@
 
 
 import REGISTER_PAGE_FORM from "./components/Register_Page_Form";
-
+import 'Register_Page_Style.css'
 import REGISTER_PAGE_TEXT from "./components/Register_Page_Text";
 import LOGO from "../logos/Logo";
 
