27289de8713f60630ac7de150de56e92804f966a
likkey <likkey1@gmail.com>
2022-01-17 01:06:17 +0100

    Merge branch 'example-react' of /Users/likkey/Desktop/front-end/example-react-test with conflicts.

---
+++ src/pages/User_Page/UserAppointmentPage/User_Appointment_Page.js
@@ -0,0 +1,17 @@
+import {StyledEngineProvider} from "@mui/material";
+import USER_APPOINTMENT_CALENDER from "./componenets/User_Appointment_Calender";
+import USER_APPOINTMENT_TEXT from "./componenets/User_Appointment_Text";
+import LOGO from "../../../components/logos/Logo";
+
+const USER_APPOINTMENT_PAGE = () => {
+  return <div>
+    <LOGO/>
+    <USER_APPOINTMENT_CALENDER/>
+
+  </div>
+
+
+
+}
+
+export default USER_APPOINTMENT_PAGE
