cfbf24d30a865be4bf09a51e43cdc10f52f8c4af
likkey <likkey1@gmail.com>
2022-01-18 16:51:34 +0100

    version 2.4 adjust the program structure.

--- src/pages/User_Page/UserAppointmentPage/User_Appointment_Page.js
+++ src/pages/User_Page/UserAppointmentPage/User_Appointment_Page.js
@@ -1,6 +1,3 @@
-import {StyledEngineProvider} from "@mui/material";
-import USER_APPOINTMENT_CALENDER from "./componenets/User_Appointment_Calender";
-import USER_APPOINTMENT_TEXT from "./componenets/User_Appointment_Text";
 import LOGO from "../../../components/logos/Logo";
 
 const USER_APPOINTMENT_PAGE = () => {
