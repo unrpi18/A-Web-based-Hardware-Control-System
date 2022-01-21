084751e90ee7210b66225e2dc02474c462e4b0a3
Haotian Wu <haotianwuwinston@gmail.com>
2022-01-21 03:33:18 +0100

    Appointment finished except for all appointments, jdocs and comments are not finished

--- src/pages/Admin_Page/Appointment_Admin/Appointment_Admin.js
+++ src/pages/Admin_Page/Appointment_Admin/Appointment_Admin.js
@@ -1,13 +1,12 @@
 import './Appointment_Admin.css'
 import LOGO from "../../../components/logos/Logo";
-import APPOINTMENT_ADMIN_BUTTON from "./Appointment_Admin_Button";
+
 import APPOINTMENT_ADMIN_TEXT from "./Appointment_Admin_Text";
 import APPOINTMENT_ADMIN_VIEW from "./Appointment_Admin_View";
 
 
 const APPOINTMENT_ADMIN = () => {
     return <div>
-        <APPOINTMENT_ADMIN_BUTTON />
         <LOGO />
         <APPOINTMENT_ADMIN_TEXT/>
         <APPOINTMENT_ADMIN_VIEW/>
