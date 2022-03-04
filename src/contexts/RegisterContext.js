9069c2413197a9de2bfee3cdc310f12979c0f22b
Haotian Wu <haotianwuwinston@gmail.com>
2022-03-04 19:46:42 +0100

    final

--- src/contexts/RegisterContext.js
+++ src/contexts/RegisterContext.js
@@ -2,4 +2,4 @@ import {createContext} from "react";
 
 
 export const UserContext = createContext(null);
-export const baseUrl = "http://f069-2a02-8071-229e-e400-1c50-6971-3348-79c1.ngrok.io";
+export const baseUrl = "http://pseteam1.dmz.teco.edu/";
